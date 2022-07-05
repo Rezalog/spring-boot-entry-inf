9e407291501af5e20125c175c206f554026b23eb
Rezalog <rladlfdnd369@naver.com>
2022-07-05 14:20:20 +0900

    [Spring DB 접근 기술 - (2)] 순수 JDBC

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,23 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) { // Spring 이 SpringConfig 를 불러올때 datasource를 주입해줌)
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -15,6 +25,7 @@ public class SpringConfig {
     }
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository(); // 인터페이스이므로 구현체 인스턴스를 생성
+//        return new MemoryMemberRepository(); // 인터페이스이므로 구현체 인스턴스를 생성
+        return new JdbcMemberRepository(dataSource);
     }
 }
